0b0de62fef694bc836c0d414e41b4b5ac332e782
touseefiqbal845 <136140163+touseefiqbal845@users.noreply.github.com>
2023-07-09 21:09:44 +0500

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -31,38 +31,8 @@ const products = [
     price: 120,
     description: "soluta ipsam autem eius necessitatibus fugiat in.",
   },
-  {
-    id: 5,
-    image: "images/cap1.jpg",
-    name: "Flexfit",
-    category: "CAPS",
-    price: 40,
-    description: "soluta ipsam autem eius necessitatibus fugiat in.",
-  },
-  {
-    id: 6,
-    image: "images/cap2.jpg",
-    name: "Goorin Bros",
-    category: "CAPS",
-    price: 30,
-    description: "soluta ipsam autem eius necessitatibus fugiat in.",
-  },
-  {
-    id: 7,
-    image: "images/cloths2.jpg",
-    name: "Rabbins",
-    category: "CLOTHES",
-    price: 600,
-    description: "soluta ipsam autem eius necessitatibus fugiat in.",
-  },
-  {
-    id: 8,
-    image: "images/cloths3.jpg",
-    name: "Jugers",
-    category: "CLOTHES",
-    price: 300,
-    description: "soluta ipsam autem eius necessitatibus fugiat in.",
-  },
+  
+    
 ];
 
 export default products;
